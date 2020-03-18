cfaf40b8143be01a5afb0cc800c8af51883985af
roberto-rodriguez <roberto.rodriguez@trustvesta.com>
2020-03-18 14:05:17 -0400

    Updating Image SDK license

--- app/src/main/java/com/kofax/samples/common/License.java
+++ app/src/main/java/com/kofax/samples/common/License.java
@@ -1,5 +1,5 @@
 package com.kofax.samples.common;
 
 public class License {
-    public static String PROCESS_PAGE_SDK_LICENSE = "&zIN;f?[AQv9bEc#O!Pf>@T06gl+FRHdnn[qlcB!PGl68Uvz7v!BTq(4(Adfj^I#dPb0WKr!!L^5xl5NUd,U![v`@pkk=m&h0#RW";
+    public static String PROCESS_PAGE_SDK_LICENSE = "N0!G89PIKf04lg5&#;nIZJ[f`]6#d(Dqfcf4[BAz7vLuF0W!&^Tr?GN5GmUnl&jv2[$l=dUE&hZ]cO`8dD!#N,N!bv^r!vUkH!dk";
 }
