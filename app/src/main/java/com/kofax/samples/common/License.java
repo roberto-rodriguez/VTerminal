1c0f351a466455154470fdea64984e585fd84189
roberto-rodriguez <roberto.rodriguez@trustvesta.com>
2020-05-28 22:10:37 -0400

    Updating Atalasoft License

--- app/src/main/java/com/kofax/samples/common/License.java
+++ app/src/main/java/com/kofax/samples/common/License.java
@@ -1,5 +1,6 @@
 package com.kofax.samples.common;
 
 public class License {
-    public static String PROCESS_PAGE_SDK_LICENSE = "N0!G89PIKf04lg5&#;nIZJ[f`]6#d(Dqfcf4[BAz7vLuF0W!&^Tr?GN5GmUnl&jv2[$l=dUE&hZ]cO`8dD!#N,N!bv^r!vUkH!dk";
+    public static String PROCESS_PAGE_SDK_LICENSE = "BUU[cR!h8g2NP6,%pn[q60AmxK@+Ff90UNDr]fkIv^Z14nbIlFWDb&SFtdzfzRv^E;L[?ldcO#YP=45#!N,px0k!!bdlj7!H(^!G";
+
 }
