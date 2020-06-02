81b1b8dec9c0fccc3e3a1e9d7f16ffe197ba42ae
roberto-rodriguez <roberto.rodriguez@trustvesta.com>
2020-06-02 09:12:44 -0400

    Fixing issue when coming back to home

--- app/src/main/java/com/voltcash/vterminal/views/home/HomeActivity.java
+++ app/src/main/java/com/voltcash/vterminal/views/home/HomeActivity.java
@@ -5,30 +5,25 @@ import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-
 import com.voltcash.vterminal.R;
-import com.voltcash.vterminal.util.Constants;
 import com.voltcash.vterminal.util.Field;
-import com.voltcash.vterminal.util.ViewUtil;
 import com.voltcash.vterminal.util.cardReader.FragmentWithCardReader;
 import com.voltcash.vterminal.views.report.ActivityReportActivity;
 import com.voltcash.vterminal.views.settings.ClerkSettingsActivity;
 import com.voltcash.vterminal.views.settings.TerminalSettingsActivity;
-import com.voltcash.vterminal.views.tx.TxActivity;
-import com.voltcash.vterminal.views.tx.TxBalanceActivity;
 import com.voltcash.vterminal.views.tx.TxBalanceFragment;
-import com.voltcash.vterminal.views.tx.TxCardToBankActivity;
 import com.voltcash.vterminal.views.tx.TxCardToBankFragment;
 import com.voltcash.vterminal.views.tx.TxFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
     private ConstraintLayout homeMenu;
+
+    private Fragment openFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +32,24 @@ public class HomeActivity extends AppCompatActivity {
 
         homeMenu = findViewById(R.id.home_menu_container);
 
+        resetHome();
+    }
+
+    private void resetHome(){
         setTitle("Voltcash Terminal");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setHomeButtonEnabled(false);
     }
 
     private void openFragment(FragmentWithCardReader fragment){
+        this.openFragment = fragment;
         homeMenu.setVisibility(View.INVISIBLE);
 
         getFragmentManager().beginTransaction().add(R.id.frame_container, fragment).commit();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     private void openTxFragment(String operation){
@@ -55,8 +61,6 @@ public class HomeActivity extends AppCompatActivity {
         openFragment(fragment);
     }
 
-//    @Override
-//    public void onBackPressed() {}
 
     public void onTxCheck(View view){
         openTxFragment("01");
@@ -90,6 +94,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent = null;
+
         switch (item.getItemId()) {
             case R.id.menu_clerk:
                 intent = new Intent(getApplicationContext(), ClerkSettingsActivity.class);
@@ -97,13 +102,27 @@ public class HomeActivity extends AppCompatActivity {
             case R.id.menu_terminal:
                 intent = new Intent(getApplicationContext(), TerminalSettingsActivity.class);
                 break;
-            case R.id.home:
-                getFragmentManager().popBackStack();
-                break;
             default:
+                onBackPressed();
+                break;
         }
 
-        startActivity(intent);
+        if(intent != null){
+            startActivity(intent);
+        }
+
         return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(this.openFragment != null){
+            resetHome();
+            getFragmentManager().beginTransaction().remove(openFragment).commit();
+            homeMenu.setVisibility(View.VISIBLE);
+            this.openFragment = null;
+        }else{
+            String a = "";
+        }
     }
 }
